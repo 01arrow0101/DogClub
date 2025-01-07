53fb76e4dc40768fa2400e70b0b7901713e266d5
AlexeyBezuglyDeveloper <112628319+01arrow0101@users.noreply.github.com>
2025-01-07 23:43:46 +0200

    new commit

--- src/views/ServicesCategory.vue
+++ src/views/ServicesCategory.vue
@@ -1,12 +1,9 @@
 <script setup>
 import { onMounted } from 'vue';
 import { useNutritionStore } from '@/stores/nutritionStore';
-import Modal from '../components/Modal/Modal.vue'
 import Nutrition from '../components/Services/Category/Nutrition.vue'
-import { ref } from 'vue'
 
 const nutritionStore = useNutritionStore()
-const isOpenModal = ref(false)
 
 
 onMounted(() => {
@@ -15,7 +12,6 @@ onMounted(() => {
 </script>
 
 <template>
-  <Modal v-if="isOpenModal"/>
   <main>
     <Nutrition />
   </main>
